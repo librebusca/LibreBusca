b93cfe12e9979e3a87cbc8be2ed429a5155e9b8b
librebusca <librebusca@proton.me>
2025-11-29 14:02:52 -0300

    Add files via upload

--- php/include.php
+++ php/include.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-// Agora você pode usar as variáveis de ambiente
 $host = $_ENV['DB_HOST'];
 $db = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
